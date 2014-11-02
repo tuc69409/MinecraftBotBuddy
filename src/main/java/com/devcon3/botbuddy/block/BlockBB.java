15e468fabdfbff0b96b5972178500d0d3820d806
CodyJamesCasey <codyjamescasey@gmail.com>
2014-11-02 13:37:02 -0500

    merged basic block branch

---
+++ src/main/java/com/devcon3/botbuddy/block/BlockBB.java
@@ -0,0 +1,39 @@
+package com.devcon3.botbuddy.block;
+
+import com.devcon3.botbuddy.reference.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
+public class BlockBB extends Block {
+
+    public BlockBB(Material material){
+
+        super(material);
+    }
+
+    public BlockBB(){
+
+        this(Material.rock);
+    }
+
+    @Override
+    public String getUnlocalizedName(){
+
+        return String.format("tile.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister){
+
+        blockIcon = iconRegister.registerIcon(String.format("%s", getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName){
+
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+}
---
+++ src/main/java/com/devcon3/botbuddy/block/BlockFlag.java
@@ -0,0 +1,11 @@
+package com.devcon3.botbuddy.block;
+
+public class BlockFlag extends BlockBB{
+
+    public BlockFlag(){
+
+        super();
+        this.setBlockName("flag");
+        this.setBlockTextureName("flag");
+    }
+}
