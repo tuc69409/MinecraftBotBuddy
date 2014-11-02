66c14a5137d924d01aa1a3540b971f53a2c9e01c
CodyJamesCasey <codyjamescasey@gmail.com>
2014-11-01 21:03:42 -0400

    added the mod class

---
+++ src/main/java/com/devcon3/botbuddy/BotBuddy.java
@@ -0,0 +1,29 @@
+package com.devcon3.botbuddy;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="BotBuddy", name = "BotBuddy", version = "1.7.10-1.0")
+public class BotBuddy {
+
+    @Mod.Instance("BotBuddy")
+    public static BotBuddy instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+
+}
