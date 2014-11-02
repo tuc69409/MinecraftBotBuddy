bc031d9df4c42338d2dd02c697eab696dcd4defc
CodyJamesCasey <codyjamescasey@gmail.com>
2014-11-02 13:33:08 -0500

    prints OreDictionary list during post initialize

--- src/main/java/com/devcon3/botbuddy/BotBuddy.java
+++ src/main/java/com/devcon3/botbuddy/BotBuddy.java
@@ -12,6 +12,7 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.oredict.OreDictionary;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class BotBuddy {
@@ -25,22 +26,47 @@ public class BotBuddy {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
         LogHelper.info("Starting Pre Initialization!");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
         FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
         ModItems.init();
         ModBlocks.init();
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
         LogHelper.info("Pre Initialization Complete!");
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event){
         LogHelper.info("Starting Initialization!");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
         LogHelper.info("Initialization Complete!");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event){
         LogHelper.info("Starting Post Initialization!");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+        for(String oreName : OreDictionary.getOreNames()){
+
+            LogHelper.info(oreName);
+        }
+
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        LogHelper.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
         LogHelper.info("Post Initialization Complete!");
     }
 
